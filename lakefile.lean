999dc6cc3e604d0d794523bb7c23b5aa3c71f488
Trent Slade <73952179+EmergentMonk@users.noreply.github.com>
2026-08-30 19:14:18 +0930

    Lean: add pinned mathlib project

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package «qsol-geo-reason» where
+  version := v!"0.1.0"
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.1"
+
+lean_lib GeoReason where
+  srcDir := "Lean"
