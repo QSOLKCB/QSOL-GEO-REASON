838f899a03342adbff5867a931de20a65f8ec024
Trent Slade <73952179+EmergentMonk@users.noreply.github.com>
2026-08-30 19:39:39 +0930

    Lean: pin mathlib to exact v4.33.1 commit

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,8 @@ package «qsol-geo-reason» where
   version := v!"0.1.0"
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.1"
+  "https://github.com/leanprover-community/mathlib4.git" @
+    "0df444a360eaa60ab8c11dca51a86af692955474"
 
 @[default_target]
 lean_lib GeoReason where
